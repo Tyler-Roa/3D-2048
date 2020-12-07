e0220eca028259c6de9c278b2db2531323a3889d
Tyler Roache <61248394+Tyler-Roa@users.noreply.github.com>
2020-12-07 12:35:28 -0700

    Update DisplayComponent.java

--- src directory/DisplayComponent.java
+++ src directory/DisplayComponent.java
@@ -6,3 +6,22 @@ This will (hopefully) display the game correctly. It should look like this
 |  |  |      |  |  |     |  |  |
 
 */
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import javax.swing.JComponent;
+import java.util.Random;
+
+public class DisplayComponent extends JComponent {
+  public void drawComponent(Graphics g) {
+    Graphics2D g2 = (Graphics2D) g;
+    
+  }
+  public int NumberGenerator() {
+    //should generate a random number, either 2 or 4
+  }
+  public int freeSpace(values[][][]) {
+     //this, given any 3D array, should be able to randomly select one of the empty spaces and return the coords, hopefully in an x,y,z format
+  }
+}
+
+//side note for myself, do more research into JFrame and JPanel
