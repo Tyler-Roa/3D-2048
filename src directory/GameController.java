22a7d2e7b7515b99410bc1a8c832f4fac37a5483
Tyler Roache <61248394+Tyler-Roa@users.noreply.github.com>
2020-12-07 12:21:09 -0700

    Update GameController.java

--- src directory/GameController.java
+++ src directory/GameController.java
@@ -1,3 +1,19 @@
 /*
 This will implement both the Scanner as well as the general math of the game. 
 */
+public class GameController() {
+  void playGame() {
+  
+      
+  }
+   void theTurn(int direction) { //direction will come from scanner
+    
+  }
+  
+  public static void main(String[] args) {
+  
+    
+    
+    
+  }
+}
