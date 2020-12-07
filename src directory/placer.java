c932ee8bf4c1247b98e4100879ae168e25dcff8e
Tyler Roache <61248394+Tyler-Roa@users.noreply.github.com>
2020-12-07 12:39:15 -0700

    Update placer.java

--- src directory/placer.java
+++ src directory/placer.java
@@ -2,3 +2,11 @@
 This file should do the math for finding random empty spaces after you move your turn, as well as finding random digits and placing them in those random spaces.
 might want to have a timer to prevent the user from spamming the hell out of this file
 */
+public class Placer() {
+  public int NumberGenerator() {
+    //should generate a random number, either 2 or 4
+  }
+  public int[] freeSpace(values[][][]) {
+     //this, given any 3D array, should be able to randomly select one of the empty spaces and return the coords, hopefully in an x,y,z format
+  }
+}
