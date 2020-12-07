ffa249e622a84f13506e7e2c3187d880c78a8a92
Tyler Roache <61248394+Tyler-Roa@users.noreply.github.com>
2020-12-07 11:36:48 -0700

    Update placer.java

--- src directory/placer.java
+++ src directory/placer.java
@@ -1,1 +1,4 @@
-
+/*
+This file should do the math for finding random empty spaces after you move your turn, as well as finding random digits and placing them in those random spaces.
+might want to have a timer to prevent the user from spamming the hell out of this file
+*/
