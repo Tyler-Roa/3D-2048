6c8a60206bb344ba7602af57332b5ac30f9f538f
Tyler Roache <61248394+Tyler-Roa@users.noreply.github.com>
2020-12-07 11:35:21 -0700

    Update DisplayComponent.java

--- src directory/DisplayComponent.java
+++ src directory/DisplayComponent.java
@@ -1,1 +1,8 @@
+/*
+This will (hopefully) display the game correctly. It should look like this
 
+|  |  |      |  |  |     |  |  |
+|  |  |      |  |  |     |  |  |
+|  |  |      |  |  |     |  |  |
+
+*/
