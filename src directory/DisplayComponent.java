b394cda094d6af137e227d9ba848cd6a0f31ce62
Tyler Roache <61248394+Tyler-Roa@users.noreply.github.com>
2020-12-07 12:35:51 -0700

    Update DisplayComponent.java

--- src directory/DisplayComponent.java
+++ src directory/DisplayComponent.java
@@ -16,12 +16,7 @@ public class DisplayComponent extends JComponent {
     Graphics2D g2 = (Graphics2D) g;
     
   }
-  public int NumberGenerator() {
-    //should generate a random number, either 2 or 4
-  }
-  public int freeSpace(values[][][]) {
-     //this, given any 3D array, should be able to randomly select one of the empty spaces and return the coords, hopefully in an x,y,z format
-  }
+  
 }
 
 //side note for myself, do more research into JFrame and JPanel
