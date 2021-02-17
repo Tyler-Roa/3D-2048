0d6c54a572288ce4d5216e7abee2475323dd6835
Tyler Roache <61248394+Tyler-Roa@users.noreply.github.com>
2021-02-16 19:48:57 -0700

    Update GameController.java

--- src directory/GameController.java
+++ src directory/GameController.java
@@ -1,19 +1,46 @@
 /*
-This will implement both the Scanner as well as the general math of the game. 
+This will implement the main method of the program as well as display our array(working on)
 */
-public class GameController() {
-  void playGame() {
-  
-      
-  }
-   void theTurn(int direction) { //direction will come from scanner
-    
-  }
-  
-  public static void main(String[] args) {
-  
-    
-    
-    
-  }
+import javax.swing.*;
+import javax.swing.JButton;
+
+public class FrameDisplay {
+	/*
+	public static void main(String args[]) {
+		JFrame framy = new JFrame();
+		//JButton endBut = new JButton("End Program"); -> necessary?
+		
+		
+		VisualComponent component = new VisualComponent();
+		JLabel arraydisplay = new JLabel("" + component.returnarray());
+		int[][][] displarray = component.returnarray();
+		
+		for(int i = 0; i < displarray.length; i++) {
+			for(int j = 0; j < displarray.length; j++) {
+				for(int k = 0; k < displarray.length; k++) {
+					System.out.print(displarray[i][j][k]);
+				}
+				System.out.println("");
+			}
+			System.out.println("");
+		}
+		
+		
+	    //framy.add(component);
+	    framy.add(arraydisplay);
+		framy.setSize(500,200);
+		framy.setTitle("3D 2048"); //this suscks
+		framy.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		
+		
+		framy.setVisible(true);
+	    	
+		    //if() {} there should be computer input for a sentinel
+	    //}
+		
+	    
+	}
+	*/
+	
 }
