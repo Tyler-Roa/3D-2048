891f8e424fda196d9c89354706be82ca329d2d14
Tyler Roache <61248394+Tyler-Roa@users.noreply.github.com>
2021-03-25 13:40:58 -0600

    Update FrameDisplay.java

--- src directory/FrameDisplay.java
+++ src directory/FrameDisplay.java
@@ -1,46 +1,78 @@
-/*
-This will implement the main method of the program as well as display our array(working on)
-*/
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.KeyStroke;
 import javax.swing.*;
-import javax.swing.JButton;
 
-public class FrameDisplay {
-	/*
-	public static void main(String args[]) {
-		JFrame framy = new JFrame();
-		//JButton endBut = new JButton("End Program"); -> necessary?
+@SuppressWarnings("serial")
+public class FrameDisplay extends JFrame{
+	VisualComponent component = new VisualComponent();
+	public FrameDisplay() {
 		
 		
-		VisualComponent component = new VisualComponent();
-		JLabel arraydisplay = new JLabel("" + component.returnarray());
-		int[][][] displarray = component.returnarray();
+			
 		
+		/*
+		 *This was a test for a console version of this program 
+		 
 		for(int i = 0; i < displarray.length; i++) {
 			for(int j = 0; j < displarray.length; j++) {
-				for(int k = 0; k < displarray.length; k++) {
+				for(int k = s0; k < displarray.length; k++) {
 					System.out.print(displarray[i][j][k]);
+					JLabel arraysec = new JLabel("" + displarray[i][j][k]);
+					add(arraysec);
 				}
-				System.out.println("");
+				System.out.print("  ");
 			}
 			System.out.println("");
 		}
+		*/
 		
 		
-	    //framy.add(component);
-	    framy.add(arraydisplay);
-		framy.setSize(500,200);
-		framy.setTitle("3D 2048"); //this suscks
-		framy.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		
-		
-		framy.setVisible(true);
-	    	
-		    //if() {} there should be computer input for a sentinel
-	    //}
-		
-	    
+		add(component);
+		component.addKeyListener(new MyKeyListener());
+		component.setFocusable(true);
+		component.requestFocus();
+		setSize(500,200);
+		setTitle("3D 2048"); //this suscks
 	}
-	*/
+	class MyKeyListener implements KeyListener
+	{ 
+	   public void keyPressed(KeyEvent event)
+	   { 
+	      String key = KeyStroke.getKeyStrokeForEvent(event).toString().replace("pressed ", ""); 
+	      switch(key) {
+	      	case "W":
+	      		component.direction(1);
+	      		break;
+	      	case "S":
+	      		component.direction(-1);
+	      		break;
+			case "A":
+				component.direction(2);
+				break;
+			case "D":
+				component.direction(-2);
+				break;
+			case "Q":
+				component.direction(3);
+				break;
+			case "E":
+				component.direction(-3);
+				break;
+			case "ENTER":
+				component.startScreen = true;
+				repaint();
+				break;
+	   		case "R":
+	   			component.reset();
+	   			repaint();
+			break;
+	      }
+	   }
+	   
+	   // Do-nothing methods
+	   public void keyReleased(KeyEvent event) {}
+	   public void keyTyped(KeyEvent event) {}
+	}
 	
 }
